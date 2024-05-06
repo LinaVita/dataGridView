f0cb7bc69a360c08067fe0b368cedd791bc85fde
slava <slava@SLAVA>
2024-05-06 18:04:13 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(570, 339);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(161, 46);
+            this.button10.TabIndex = 21;
+            this.button10.Text = "Права частина масиву";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(403, 339);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(161, 46);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "Верхня частина масиву";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(236, 303);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(161, 46);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "Побічна діагональ масиву";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(69, 303);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(161, 46);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Головна діагональ масиву";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(570, 407);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(161, 46);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Ліва частина масиву";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(403, 407);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 46);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Нижня частина масиву";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(236, 445);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 46);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Під діагоналлю";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(236, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 46);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Над діагоналлю";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 46);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Під діагоналлю";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Над діагоналлю";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(80, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(630, 297);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 544);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView2.RowCount = 10;
+            dataGridView2.ColumnCount = 10;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.AllowUserToResizeColumns = false;
+            dataGridView2.AllowUserToResizeRows = false;
+        }
+        private void ColorArea(Func<int, int, bool> condition, Color color)
+        {
+            for (int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                {
+                    if (condition(i, j))
+                    {
+                        dataGridView2.Rows[i].Cells[j].Style.BackColor = color;
+                    }
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i == j, Color.Red);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i < j, Color.Green);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i > j, Color.Blue);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i + j == dataGridView2.RowCount - 1, Color.Yellow);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i + j < dataGridView2.RowCount - 1, Color.Orange);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i + j > dataGridView2.RowCount - 1, Color.Purple);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i < dataGridView2.RowCount / 2, Color.Pink);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => i >= dataGridView2.RowCount / 2, Color.Brown);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => j < dataGridView2.ColumnCount / 2, Color.Cyan);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ColorArea((i, j) => j >= dataGridView2.ColumnCount / 2, Color.Magenta);
+        }
+    }
+
+}
